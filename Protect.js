1980fc52321c0f49eb3886876f29307c74b0613a
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-21 07:01:12 +0600

    moongose model done

---
+++ Protect.js
@@ -0,0 +1,21 @@
+
+const jwt = require('jsonwebtoken');
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+export default verifyJWT
